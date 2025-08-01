d22b1d61e6b55b9423d678bb02e84ef914e591c6
Aishwarya Waje <aishux2223@gmail.com>
2025-08-01 20:01:42 +0530

    Fixed

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
@@ -13,10 +13,10 @@ import Admin from "./pages/Admin";
 import AdminLogin from "./pages/AdminLogin";
 import RequireAdmin from "./pages/RequireAdmin";
 import { getAdminUser } from "./utils/AuthUtils";
-import { Navigate } from "react-router-dom";
 
 const App = () => {
   const isAdmin = getAdminUser();
+
   return (
     <Router>
       <Routes>
@@ -29,28 +29,19 @@ const App = () => {
             </RequireAdmin>
           }
         />
-
         <Route
-          path="/*"
-          element={
-            isAdmin ? (
-              <Navigate to="/admin" replace />
-            ) : (
-              <Layout>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/search" element={<SearchResults />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/add-book" element={<AddBook />} />
-                  <Route path="/my-books" element={<MyBooks />} />
-                  <Route path="/messages" element={<Messages />} />
-                  <Route path="/wishlist" element={<Wishlist />} />
-                </Routes>
-              </Layout>
-            )
-          }
-        />
+          path="/"
+          element={isAdmin && isAdmin.isAdmin ? <Navigate to="/admin" replace /> : <Layout />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<SearchResults />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="add-book" element={<AddBook />} />
+          <Route path="my-books" element={<MyBooks />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="wishlist" element={<Wishlist />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
